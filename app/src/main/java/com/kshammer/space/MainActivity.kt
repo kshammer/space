b9372434717f867daf9cc9662b9e6a418404aaa3
kshammer <kshammerschmidt@gmail.com>
2022-01-21 09:35:53 -0800

    Added api call

--- app/src/main/java/com/kshammer/space/MainActivity.kt
+++ app/src/main/java/com/kshammer/space/MainActivity.kt
@@ -2,10 +2,29 @@ package com.kshammer.space
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        run("https://api.github.com/users/Evin1-/repos")
+    }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+        })
     }
 }
